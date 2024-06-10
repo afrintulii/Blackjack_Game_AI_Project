090726eab1b365904635606b4aadf8d862210a0d
tuli1907019@stud.kuet.ac.bd <tuli1907019@stud.kuet.ac.bd>
2024-06-10 15:57:49 +0600

    initial commit

---
+++ Blackjack_version1.py
@@ -0,0 +1,27 @@
+#import necessary libraries
+
+import copy
+import random
+import pygame
+import os
+
+
+#game variables
+card = [ '2', '3','4','5','6','7','8','9','10','J','Q','K','A']
+
+values = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10":10,
+    "A": 11,
+    "J": 10,
+    "K": 10,
+    "Q": 10,
+
+}
